b858409b318565322ccc9e639d58f2c086aa28d0
wangxinlei <1241833750@qq.com>
2020-08-24 16:38:52 +0800

    自动登录

--- lib/Person.dart
+++ lib/Person.dart
@@ -1,6 +1,17 @@
+import 'package:employment/Login/LoginPager.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Pearson extends StatelessWidget {
+
+  Future logOut(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('status', false);
+    Navigator.of(context).pushAndRemoveUntil(
+        new MaterialPageRoute(builder: (context) => LoginPage()
+        ), (route) => route == null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +33,7 @@ class Pearson extends StatelessWidget {
           splashColor: Colors.black,
           child: (Text("退出登录")),
           onPressed: () {
-            print("sss");
+            logOut(context);
           },
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,15 @@
 
 import 'package:flutter/material.dart';
-//import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'Login/LoginPager.dart';
-
 import 'Tabs.dart';
-//
-//void main() => runApp(new MyApp1());
-//Future<void> main() async {
-//  SharedPreferences prefs = await SharedPreferences.getInstance();
-//  var email = prefs.getString('usename');
-//  print(email);
-//  runApp(MaterialApp(home: email == null ? MyApp1() : MyApp()));}
-void main(){
-  String email = null;
-  if(email  ==   null){
-    runApp(new MyApp1());
-  }
-  else{
-    runApp(new MyApp());
-  }}
-
-//void main() {
-//  runApp(MyApp1());
-//  if (Platform.isAndroid) {
-//    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
-//    SystemUiOverlayStyle systemUiOverlayStyle =
-//    SystemUiOverlayStyle(
-//        statusBarColor: Colors.transparent,
-//        statusBarIconBrightness: Brightness.dark
-//    );
-//    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
-//
-//  }
-//}
 
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var status = prefs.getBool('status');
+  runApp(MaterialApp(home: status == true ? Tabs() : LoginPage(),));
 }
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Welcome to Flutter',
-      home: Tabs(),
-
-    );
-  }
-}
-
-class MyApp1 extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter_LoginPlugin',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
-  }
-
-}
 
